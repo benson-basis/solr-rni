61b1201b3776a04487a8deab344137a6e1ccf634
Benson Margulies <benson@basistech.com>
2015-02-10 15:14:03 +0000

    Start somewhere.

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+bt_root=/data/root_7.12.0_7.12.1
+libdir=$bt_root/rlp/lib/amd64-glibc25-gcc41
+ehcache=/data/solr+rni/ehcache.xml
+echo bt_root: $bt_root
+echo libdir: $libdir
+/data/solr-4.10.3/bin/solr start -s $PWD/solr_home -a "-Djava.library.path=$libdir -Dbt.root=$bt_root -Dbt.rni.ehcacheConfig=$ehcache" "$@"
+
