ae44fabb5f1bcc875f2a5e6a07a44adc548b3626
Benson Margulies <benson@basistech.com>
2015-02-11 21:12:27 +0000

    Move to trunk.

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-bt_root=/data/root_7.12.0_7.12.1
-libdir=$bt_root/rlp/lib/amd64-glibc25-gcc41
+bt_root=/data/root_java_45948
 ehcache=/data/solr+rni/ehcache.xml
 echo bt_root: $bt_root
-echo libdir: $libdir
-/data/solr-4.10.3/bin/solr start -m 200g -s $PWD/solr_home -a "-Djava.library.path=$libdir -Dbt.root=$bt_root -Dbt.rni.ehcacheConfig=$ehcache" "$@"
+jmx="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dbt.rni.ehcache.jmx=true"
+/data/solr-4.10.3/bin/solr start -m 200g -s $PWD/solr_home -a "-Dbt.root=$bt_root -Dbt.rni.ehcacheConfig=$ehcache $jmx" "$@"
 
