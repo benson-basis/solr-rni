395b679e3baf916afcd1642950c9d635dfd875c1
Benson Margulies <benson@basistech.com>
2015-02-23 17:29:43 +0000

    More stuff.

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-bt_root=/data/root_java_45966
+bt_root=/data/root_java_45976
 ehcache=/data/solr+rni/ehcache.xml
 echo bt_root: $bt_root
-jmx="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dbt.rni.ehcache.jmx=true"
+#jmx="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dbt.rni.ehcache.jmx=true"
 /data/solr-4.10.3/bin/solr start -m 200g -s $PWD/solr_home -a "-Dbt.root=$bt_root -Dbt.rni.ehcacheConfig=$ehcache $jmx" "$@"
 
