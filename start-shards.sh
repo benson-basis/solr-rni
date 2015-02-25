364ac2410ad2c896874509d840f683ce7069fd1b
Benson Margulies <benson@basistech.com>
2015-02-25 07:35:25 -0500

    cloud studd.

---
+++ start-shards.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# initial setup to make 8 shareds on this node.
+bt_root=/data/root_java_46032
+ehcache=/data/solr+rni/ehcache.xml
+
+gcopts="-XX:NewRatio=3 -XX:SurvivorRatio=4 -XX:TargetSurvivorRatio=90 -XX:MaxTenuringThreshold=8 -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:ConcGCThreads=4 -XX:ParallelGCThreads=4 -XX:+CMSScavengeBeforeRemark -XX:PretenureSizeThreshold=64m -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=50 -XX:CMSMaxAbortablePrecleanTime=6000 -XX:+CMSParallelRemarkEnabled -XX:+ParallelRefProcEnabled -XX:CMSFullGCsBeforeCompaction=1 -XX:CMSTriggerPermRatio=80 -verbose:gc -XX:+PrintHeapAtGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+PrintTenuringDistribution -XX:+PrintGCApplicationStoppedTime -Xloggc:logs/solr_gc.log -XX:OnOutOfMemoryError=/data/solr-4.10.3/bin/oom_solr.sh"
+
+solropts="-DSTOP.PORT=7983 -DSTOP.KEY=solrrocks -Djetty.port=8983 -Dsolr.solr.home=/data/solr+rni/cloud_solr_home -Dsolr.install.dir=/data/solr-4.10.3 -Duser.timezone=UTC -Djava.net.preferIPv4Stack=true"
+
+rniopts="-Dbt.root=/data/root_java_46032 -Dbt.rni.ehcacheConfig=/data/solr+rni/ehcache.xml"
+
+echo bt_root: $bt_root
+DIR=$PWD
+cd ../solr-4.10.3/example
+java -Xmx200g $gcopts $rniopts $solropts -DzkRun -jar start.jar
+
--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ bt_root=/data/root_java_46032
 ehcache=/data/solr+rni/ehcache.xml
 echo bt_root: $bt_root
 #jmx="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9010 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dbt.rni.ehcache.jmx=true"
-/data/solr-4.10.3/bin/solr start -m 200g -s $PWD/solr_home -a "-Dbt.root=$bt_root -Dbt.rni.ehcacheConfig=$ehcache $jmx" "$@"
+bash -x /data/solr-4.10.3/bin/solr start -m 200g -s $PWD/solr_home -a "-Dbt.root=$bt_root -Dbt.rni.ehcacheConfig=$ehcache $jmx" "$@"
 
